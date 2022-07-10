f112db461a41339fadbfbf1c472e0bbcad833673
moonthegreat969 <100451541+moonthegreat969@users.noreply.github.com>
2022-07-10 04:01:54 +0100

    Add files via upload
    ssjsjsj

---
+++ obabeats.js
@@ -0,0 +1,19 @@
+function myFunction() {
+    var x = document.getElementById("hidden") ;
+    if (x.style.visibility === 'hidden') {
+        x.style.visibility = "visible";
+    }
+    else {
+    x.style.visibility = "hidden"
+    }
+}
+
+function myFunction2() {
+    var x = document.getElementById("hiddensmallmobile") ;
+    if (x.style.visibility === 'hidden') {
+        x.style.visibility = "visible";
+    }
+    else {
+    x.style.visibility = "hidden"
+    }
+}
